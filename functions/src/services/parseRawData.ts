14d0e48b641e20f58efb274f155057a65ea85477
satoshi <129905427+chirushiyo@users.noreply.github.com>
2026-08-26 23:39:49 +0900

    fix: BLE AD構造パース時の16進文字列の大文字, 小文字不一致を修正

--- functions/src/services/parseRawData.ts
+++ functions/src/services/parseRawData.ts
@@ -6,24 +6,21 @@ export const parseRawData = (a: string): [string, string] => {
     let companyId: string = "";
     let nearbyInfo: string = "";
 
-    for (const data of a) {
-        let companyId = "";
-        let nearbyInfo = "";
         let now_location = 0;
 
-        while (now_location < data.length / 2) {
-            const length = parseInt(getByte(data, now_location), 16);// lengthは、BLEの[[長さ][型][中身]...(繰り返し)]のスキーマうち、[長さ]を抽出する。[長さ]は[型][中身]の合計の長さを示す。
+        while (now_location < a.length / 2) {
+            const length = parseInt(getByte(a, now_location), 16);// lengthは、BLEの[[長さ][型][中身]...(繰り返し)]のスキーマうち、[長さ]を抽出する。[長さ]は[型][中身]の合計の長さを示す。
             if (length === 0) break;// [長さ]が0の場合、[型][中身]が存在しないので、その時点でパースを終了する。
-            const datatype = getByte(data, now_location + 1);
-            if (datatype === "ff") {// [型]が0xffの場合、[中身]はCompany Specific Dataとわかる。
-                companyId = getByte(data, now_location + 3) + getByte(data, now_location + 2);
+            const datatype = (getByte(a, now_location + 1)).toUpperCase();// datatypeは、BLEの[[長さ][型][中身]...(繰り返し)]のスキーマうち、[型]を抽出する。[型]は[中身]のデータの種類を示す。
+            if (datatype === "FF") {// [型]が0xffの場合、[中身]はCompany Specific Dataとわかる。
+                companyId = (getByte(a, now_location + 3) + getByte(a, now_location + 2)).toUpperCase();
                 if (companyId === "004C") {// CompanyIDが"0x004C"の場合、Specific dataがApple continutityとわかる。
                     let apple_location = now_location + 4;// Apple continutityの[中身]の先頭位置を示す。
                     const end = now_location + 1 + length;
                     // Company Specific dataのApple continutityは[[型][長さ][中身]...(繰り返し)]と続いていく。
                     while (apple_location < end) {
-                        const apple_type = getByte(data, apple_location);
-                        const apple_length = parseInt(getByte(data, apple_location + 1), 16);
+                        const apple_type = (getByte(a, apple_location).toUpperCase());// Apple continutityの[型]を抽出する。[型]は[中身]のデータの種類を示す。
+                        const apple_length = parseInt(getByte(a, apple_location + 1), 16);
                         if (apple_type === "10") {// Apple continutityの[型]が0x10の場合、[中身]はNearby Infoとわかる。
                             nearbyInfo = "10";
                         }
@@ -34,8 +31,5 @@ export const parseRawData = (a: string): [string, string] => {
             }
             now_location = now_location + 1 + length;
         }
-        companyId += companyId;
-        nearbyInfo += nearbyInfo;
-    }
     return [companyId, nearbyInfo];
 };
