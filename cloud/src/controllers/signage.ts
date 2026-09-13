82478122a1a7cd88e6a020252ff2720829ff8c76
satoshi <129905427+chirushiyo@users.noreply.github.com>
2026-09-13 09:26:37 +0900

    feat: ヘッダーキーでの認証を行ったあと、広報のリンクやid等を含めた配列の集まりを配列で返すエンドポイントを実装。

--- cloud/src/controllers/signage.ts
+++ cloud/src/controllers/signage.ts
@@ -1,7 +1,8 @@
 import { Hono } from "hono";
 import { congestion } from "../services/congestion";
 import { congestion_history } from "../services/congestion";
-
+import { listPublishedPrAssets } from "../services/PublicRelations";
+import { signageAuthMiddleware } from "../middlewares/signage_auth";
 
 export const congestionRoute = new Hono();
 
@@ -12,3 +13,8 @@ congestionRoute.get("/getCongestion", async (c) => {
 congestionRoute.get("/getCongestionHistory", async (c) => {
     return congestion_history(c);
 })
+
+congestionRoute.get("/signage/assets", signageAuthMiddleware, async (c) => {
+  const assets = await listPublishedPrAssets();
+  return c.json({ assets });
+});
