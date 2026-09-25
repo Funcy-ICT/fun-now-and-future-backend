fa98120e94d837345a0266dca4539d47d0e11949
Satoshi0102 <129905427+chirushiyo@users.noreply.github.com>
2026-09-25 11:52:20 +0900

    fix: apiキー直書きを、cloud runの環境変数ESP32_API_KEYから取得する形に変更。パブリックリポジトリで、キーが丸見えであるため

--- cloud/src/middlewares/sensor_auth.ts
+++ cloud/src/middlewares/sensor_auth.ts
@@ -1,6 +1,6 @@
-const VALID_API_KEY = "funcy_esp32_secret_key_2026";
+const VALID_API_KEY = process.env.ESP32_API_KEY;
 
-export const sensorAuthMiddleware = async (key: string | undefined) => {
+export const sensorAuthMiddleware = (key: string | undefined) => {
 
   if (!key || key !== VALID_API_KEY) {
     return 0;
